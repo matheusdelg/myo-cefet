77c49cfe055427a0a8070ea1fb7095fca59a50c8
matheusdelg <matheus.delg@hotmail.com>
2017-06-25 16:32:03 -0300

    Create defs.h

---
+++ codes/C/OpenGL/defs.h
@@ -0,0 +1,20 @@
+#define SCREENX  800 // largura (em pixels) da tela  
+#define SCREENY  600 // altura (em pixels) da tela
+ 
+#define MINX    -100 // x mínimo
+#define MAXX     100 // x máximo
+#define MINY    -100 // y mínimo
+#define MAXY     100 // y máximo
+ 
+#define WHITE (1.0f,1.0f,1.0f,1.0f)
+#define GRAY (0.8f,0.8f,0.8f)
+#define TITLE "Grafico"
+#define open fopen
+#define DATAFILE ("myo.emg.emg","r")
+#define CHECK_RUN (pf!=NULL)
+#define ESC 27
+#define ENTER 13
+ 
+#define SAMPLES  (MAXX - MINX) // quantidade de amostras
+#define TRUE  1
+#define FALSE 0
